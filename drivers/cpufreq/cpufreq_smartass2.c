fb67500faa1416f97c2120433d2c982a24a9f12b
mericon <graf.carsten@gmail.com>
2012-07-12 18:56:35 +0200

    tweaked smartass2

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -43,7 +43,7 @@
 * towards the ideal frequency and slower after it has passed it. Similarly,
 * lowering the frequency towards the ideal frequency is faster than below it.
 */
-#define DEFAULT_AWAKE_IDEAL_FREQ 918000
+#define DEFAULT_AWAKE_IDEAL_FREQ 864000
 static unsigned int awake_ideal_freq;
 
 /*
@@ -52,7 +52,7 @@ static unsigned int awake_ideal_freq;
 * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
 * also when suspended).
 */
-#define DEFAULT_SLEEP_IDEAL_FREQ 384000
+#define DEFAULT_SLEEP_IDEAL_FREQ 192000
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -60,7 +60,7 @@ static unsigned int sleep_ideal_freq;
 * Zero disables and causes to always jump straight to max frequency.
 * When below the ideal freqeuncy we always ramp up to the ideal freq.
 */
-#define DEFAULT_RAMP_UP_STEP 270000
+#define DEFAULT_RAMP_UP_STEP 384000
 static unsigned int ramp_up_step;
 
 /*
@@ -68,40 +68,40 @@ static unsigned int ramp_up_step;
 * Zero disables and will calculate ramp down according to load heuristic.
 * When above the ideal freqeuncy we always ramp down to the ideal freq.
 */
-#define DEFAULT_RAMP_DOWN_STEP 270000
+#define DEFAULT_RAMP_DOWN_STEP 384000
 static unsigned int ramp_down_step;
 
 /*
 * CPU freq will be increased if measured load > max_cpu_load;
 */
-#define DEFAULT_MAX_CPU_LOAD 50
+#define DEFAULT_MAX_CPU_LOAD 85
 static unsigned long max_cpu_load;
 
 /*
 * CPU freq will be decreased if measured load < min_cpu_load;
 */
-#define DEFAULT_MIN_CPU_LOAD 25
+#define DEFAULT_MIN_CPU_LOAD 50
 static unsigned long min_cpu_load;
 
 /*
 * The minimum amount of time to spend at a frequency before we can ramp up.
 * Notice we ignore this when we are below the ideal frequency.
 */
-#define DEFAULT_UP_RATE_US 48000;
+#define DEFAULT_UP_RATE_US 95000;
 static unsigned long up_rate_us;
 
 /*
 * The minimum amount of time to spend at a frequency before we can ramp down.
 * Notice we ignore this when we are above the ideal frequency.
 */
-#define DEFAULT_DOWN_RATE_US 99000;
+#define DEFAULT_DOWN_RATE_US 40000;
 static unsigned long down_rate_us;
 
 /*
 * The frequency to set when waking up from sleep.
 * When sleep_ideal_freq=0 this will have no effect.
 */
-#define DEFAULT_SLEEP_WAKEUP_FREQ 99999999
+#define DEFAULT_SLEEP_WAKEUP_FREQ 384000
 static unsigned int sleep_wakeup_freq;
 
 /*
