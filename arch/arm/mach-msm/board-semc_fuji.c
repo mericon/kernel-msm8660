dc494e5859ef99de96f0f3c21ef54fdd591f2482
mericon <graf.carsten@gmail.com>
2012-07-30 12:05:16 +0200

    disabled RTC write

--- arch/arm/mach-msm/board-semc_fuji.c
+++ arch/arm/mach-msm/board-semc_fuji.c
@@ -4614,7 +4614,7 @@ static struct pm8xxx_vibrator_platform_data pm8058_vib_pdata = {
 };
 
 static struct pm8xxx_rtc_platform_data pm8058_rtc_pdata = {
-	.rtc_write_enable       = true,
+	.rtc_write_enable       = false,
 	.rtc_alarm_powerup	= false,
 };
 
